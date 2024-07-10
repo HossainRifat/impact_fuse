d9e8e06b8879ac61d5151884cf25e04799fe7987
HossainRifat <rh140025@gmail.com>
2024-07-10 23:30:36 +0600

    video url added

--- resources/views/site/event.blade.php
+++ resources/views/site/event.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    @keyframes glow {
+        0% { box-shadow: 0 0 2px 2px #df1b1b99; }
+        25% { box-shadow: 0 0 3px 3px #df1b1b; }
+        50% { box-shadow: 0 0 4px 4px #df1b1b; }
+        75% { box-shadow: 0 0 3px 3px #df1b1b; }
+        100% { box-shadow: 0 0 2px 2px #df1b1b99; }
+    }
+    .glow-effect {
+        animation: glow 1.5s cubic-bezier(0.4, 0, 0.2, 1) infinite;
+    }
+</style>
 <div class="container-fluid page-overlay header-margin">
     <div class="container-fluid body-section">
         <div class="container my-5">
@@ -28,9 +40,21 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-12 mb-2 blog-image">
-                    <img src="{{get_image($event->photo?->photo)}}" alt="">
-                </div>
+                @if($event->video_url)
+                    <div class="col-12 mb-2 blog-image" style="position: relative; display: inline-block;">
+                        <img src="{{ get_image($event->photo?->photo) }}" alt="" 
+                                style="width: 100%; display: block;">
+                        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#exampleModal" 
+                            style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+                            <img src="{{asset('images/assets/play-alt.png')}}" alt="Play" 
+                                    style="width: 64px; height: 64px;" class="glow-effect">
+                        </a>
+                    </div>
+                @else
+                    <div class="col-12 mb-2 blog-image" style="position: relative; display: inline-block;">
+                        <img src="{{get_image($event->photo?->photo)}}" alt="">
+                    </div>
+                @endif
                 <div class="col-md-12">
                     <p class="fs-6 fw-bold text-secondary">Organized by ImpactFuse | {{ \Carbon\Carbon::parse($event->start_date)->format('d M, Y') }}</p>
                 </div>
@@ -90,4 +114,22 @@
         </div>
     </div>
 </div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">{{$event->title}}</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            @if($event->video_url)
+                <div class="ratio ratio-16x9">
+                    <iframe src="{{$event->video_url}}" allowfullscreen></iframe>
+                </div>
+            @endif
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
